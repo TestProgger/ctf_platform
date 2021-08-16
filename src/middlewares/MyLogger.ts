b41b901ff945d0d5d26ad9ae99b042ceb0f722f0
TestProgger <gamersteam_dubki@mail.ru>
2021-08-16 12:01:46 +0300

    Optimization: In logger added IIFO , etc

--- src/middlewares/MyLogger.ts
+++ src/middlewares/MyLogger.ts
@@ -2,9 +2,10 @@ import express , { Request , Response , NextFunction} from 'express';
 import fs from 'fs';
 
 const LoggerDir = "./logging/";
+const date = new Date();
+
 
 const getFormatedTime = () => {
-    const date = new Date();
     return {
         date : date.toLocaleString().split(',')[0].trim().replace(/[:\/\-]/gmi , '_'),
         time : date.toLocaleString().split(',')[1].trim()
@@ -12,7 +13,6 @@ const getFormatedTime = () => {
 }
 
 const parseXAuthHeader = ( request :  Request ) => {
-
     const xAuthHeader = request.headers["x-auth-token"];
     return xAuthHeader ?  JSON.parse( Buffer.from( xAuthHeader as string , 'base64' ).toString() ) : null
 }
@@ -24,25 +24,30 @@ export const Logger = () =>  ( request : Request , response : Response,
 {
     if( !fs.existsSync( LoggerDir ) ){fs.mkdirSync( LoggerDir );}
 
-    const formatedTime : { time : string , date : string } = getFormatedTime();
-    const  logFileName = `${LoggerDir}/log_${formatedTime.date}.json`;
-    const timeStamp : string = formatedTime.date + "-" + formatedTime.time;
-    const method : string = request.method;
-    const ip : string = request.ip;
-    const path : string =  request.path;
-    const protocol : string =  request.protocol;
-    const  xAuthHeader =  parseXAuthHeader(request);
-    fs.appendFile( logFileName ,
-        JSON.stringify(
-            {
-                timeStamp ,
-                method  ,
-                ip ,
-                path ,
-                protocol ,
-                gradeBookNumber: xAuthHeader?.gradeBookNumber ,
-                userAgent : request.headers["user-agent"]
+    (async () => {
+            const formatedTime : { time : string , date : string } = getFormatedTime();
+            const  logFileName = `${LoggerDir}/log_${formatedTime.date}.json`;
+            const timeStamp : string = formatedTime.date + "-" + formatedTime.time;
+            const method : string = request.method;
+            const ip : string = request.ip;
+            const path : string =  request.path;
+            const protocol : string =  request.protocol;
+            const  xAuthHeader =  parseXAuthHeader(request);
+            fs.appendFile( logFileName ,
+                JSON.stringify(
+                    {
+                        timeStamp ,
+                        method  ,
+                        ip ,
+                        path ,
+                        protocol ,
+                        gradeBookNumber: xAuthHeader?.gradeBookNumber ,
+                        userAgent : request.headers["user-agent"]
 
-            } ) + "\n" , (err) => err ? console.error( err ) : null );
+                    } ) + "\n" , (err) => err ? console.error( err ) : null );
+        }
+    )();
+
+
     next();
 }
