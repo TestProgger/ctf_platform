5d38021515ae2412d4d76b6553da3621d4d442bc
TestProgger <gamersteam_dubki@mail.ru>
2021-08-18 15:39:20 +0300

    B rem

--- client/src/hooks/useRoutes.tsx
+++ client/src/hooks/useRoutes.tsx
@@ -1,5 +1,5 @@
 import React   from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch  } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import AboutDevelopersPage from '../pages/AboutDevelopersPage';
--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,6 @@ app.use( helmet() );
 app.use( express.json() );
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use( Logger() );
 
 app.use("/public" , express.static("public") );
