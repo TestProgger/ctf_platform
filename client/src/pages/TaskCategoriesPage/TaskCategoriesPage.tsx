79fbac629b8b5f8dfbad7510ae05e0125e5f7647
TestProgger <gamersteam_dubki@mail.ru>
2021-09-15 09:02:01 +0300

    Component TaskCategoriesPage translated

--- client/src/pages/TaskCategoriesPage/TaskCategoriesPage.tsx
+++ client/src/pages/TaskCategoriesPage/TaskCategoriesPage.tsx
@@ -19,14 +19,14 @@ function TaskCategoriesPage(){
         <div className="task_categories__app_shadow">
             <div className = "task_categories__container">
                 <div className = "row justify-content-center" >
-                    <TaskCategoryCard category  = "joy" title = "Joy" icon = {joyIcon}  description = "Различные развлекательные задачи с выбором ответа" />
-                    <TaskCategoryCard category  = "reverse" title = "Reverse" icon = {reverseIcon}  description = "Задачи в которых придется разобраться в исполняемых файлах" />
-                    <TaskCategoryCard category  = "stegano" title = "Stegano" icon = {steganoIcon}  description = "Ответы спрятаны на видном месте" />
+                    <TaskCategoryCard category  = "joy" title = "Joy" icon = {joyIcon}  description = "Simple tasks without straining your brain" />
+                    <TaskCategoryCard category  = "reverse" title = "Reverse" icon = {reverseIcon}  description = "Tasks in which you have to figure out executable files" />
+                    <TaskCategoryCard category  = "stegano" title = "Stegano" icon = {steganoIcon}  description = "The answers are hidden in a prominent place" />
                 </div>
                 <div className = "row justify-content-center" >
-                    <TaskCategoryCard category  = "crypto" title = "Crypto" icon = {cryptoIcon}  description = "Раздел будет интересен тем кто хочет узнать чужие секреты" />
-                    <TaskCategoryCard category  = "web" title = "WEB" icon = {webIcon}  description = "Хотели когда нибудь взломать ВК?" />
-                    <TaskCategoryCard category  = "linux" title = "Linux" icon = {linuxIcon}  description = "Красноглазики вошли в чат" />
+                    <TaskCategoryCard category  = "crypto" title = "Crypto" icon = {cryptoIcon}  description = "This section will be of interest to those who want to know other people's secrets" />
+                    <TaskCategoryCard category  = "web" title = "WEB" icon = {webIcon}  description = "Ever wanted to hack into VK?" />
+                    <TaskCategoryCard category  = "linux" title = "Linux" icon = {linuxIcon}  description = "Red-eyes entered the chat" />
                 </div>
             </div>
         </div>
